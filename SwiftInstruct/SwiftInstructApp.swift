3d645ffd9126a3bc9e5b6fd2f4c6c85dfff76d2a
brianbmobaik <89275270+brianbmobaik@users.noreply.github.com>
2023-11-18 09:29:58 -0800

    Initial Commit

---
+++ SwiftInstruct/SwiftInstructApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftInstructApp.swift
+//  SwiftInstruct
+//
+//  Created by FBI Man on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftInstructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
