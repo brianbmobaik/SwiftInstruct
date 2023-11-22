0a5ee9559ce770664aa6538a57a1a21eef01cd54
Valeria <49655194+Valerias81615@users.noreply.github.com>
2023-11-22 14:01:12 -0800

    Update LogInView.swift

--- SwiftInstruct/Views/LogInView.swift
+++ SwiftInstruct/Views/LogInView.swift
@@ -8,17 +8,58 @@ struct LogInView: View {
     @State var password = ""
     
     var body: some View {
-        VStack {
-            Form {
-                TextField("Email Address", text: $email)
-                SecureField("Password", text: $password)
+        VStack(alignment: .leading) {
+            Text("Welcome to SwiftInstruct")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .padding()
+            
+            ZStack(alignment: .leading) {
+                if email.isEmpty {
+                    Text("Email")
+                        .foregroundColor(.gray)
+                        .bold()
+                        .padding(.leading, 22)
+                }
+                
+                TextField("", text: $email)
+                    .foregroundColor(.black)
+                    .textFieldStyle(.plain)
+                    .padding()
             }
+            Rectangle()
+                .frame(width: 350, height: 1)
+                .foregroundColor(.black)
+                .padding(.top, -20)
+                .offset(x: 20)
+            
+            ZStack(alignment: .leading) {
+                if password.isEmpty {
+                    Text("Password")
+                        .foregroundColor(.gray)
+                        .bold()
+                        .padding(.leading, 22)
+                }
+                
+                SecureField("", text: $password)
+                    .foregroundColor(.black)
+                    .textFieldStyle(.plain)
+                    .padding()
+            }
+            Rectangle()
+                .frame(width: 352, height: 1)
+                .foregroundColor(.black)
+                .padding(.top, -33)
+                .offset(x: 6)
+            .padding()
         }
     }
-}
-
-struct LogInView_Previews: PreviewProvider {
-    static var previews: some View {
-        LogInView()
+    
+    
+    struct LogInView_Previews: PreviewProvider {
+        static var previews: some View {
+            LogInView()
+        }
     }
 }
