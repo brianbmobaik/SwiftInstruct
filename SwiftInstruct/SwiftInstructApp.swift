4fb3b2da6b14d9da2efff891ab4bf79235e6bcb0
Valeria <49655194+Valerias81615@users.noreply.github.com>
2023-11-22 17:41:32 -0800

    Update SwiftInstructApp.swift

--- SwiftInstruct/SwiftInstructApp.swift
+++ SwiftInstruct/SwiftInstructApp.swift
@@ -4,9 +4,18 @@ import SwiftUI
 
 @main
 struct SwiftInstructApp: App {
+        @State private var isLoggedIn = false  // Assuming initially the user is not logged in
     var body: some Scene {
         WindowGroup {
-            ModulesListView(modules: Modules.sampleModule)
+            if isLoggedIn {
+                // If the user is logged in, show the ModulesListView
+                ModulesListView(modules: Modules.sampleModule)
+            } else {
+                // If the user is not logged in, show the MainMenuView
+                MainMenuView(loggedIn: $isLoggedIn)
+            }
+            //MainMenuView()
+            //ModulesListView(modules: Modules.sampleModule)
         }
     }
 }
