c84b54b1deb61066f78f641c4c49c563d742a590
brianbmobaik <89275270+brianbmobaik@users.noreply.github.com>
2023-12-09 21:48:11 -0800

    Revert "Valeria"

--- SwiftInstruct/SwiftInstructApp.swift
+++ SwiftInstruct/SwiftInstructApp.swift
@@ -4,18 +4,9 @@ import SwiftUI
 
 @main
 struct SwiftInstructApp: App {
-        @State private var isLoggedIn = false  // Assuming initially the user is not logged in
     var body: some Scene {
         WindowGroup {
-            if isLoggedIn {
-                // If the user is logged in, show the ModulesListView
-                ModulesListView(modules: Modules.sampleModule)
-            } else {
-                // If the user is not logged in, show the MainMenuView
-                MainMenuView(loggedIn: $isLoggedIn)
-            }
-            //MainMenuView()
-            //ModulesListView(modules: Modules.sampleModule)
+            ModulesListView(modules: Modules.sampleModule)
         }
     }
 }
