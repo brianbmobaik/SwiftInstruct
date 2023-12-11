9b939f1e10aafe7fddbf334308dc585f48908045
Joshua Villareal <112414450+Onceuuu@users.noreply.github.com>
2023-12-11 01:55:33 -0800

    Adding Shadan's Views files

---
+++ SwiftInstruct/Views/DataStructuresDetailView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct DataStructuresDetailView: View {
+    var modules: [ModuleData] = DataStructuresData.modules
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    ForEach(modules, id: \.title) { module in
+                        categoryView(title: module.title, description: module.description, example: module.example)
+                    }
+                }
+                .padding()
+            }
+        }
+        .padding(.horizontal)
+        // .navigationBarTitle("Data Structures")
+    }
+
+    func categoryView(title: String, description: String, example: String) -> some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(title)
+                .font(.headline)
+
+            Text(description)
+                .foregroundColor(.secondary)
+                .fixedSize(horizontal: false, vertical: true)
+
+            Spacer()
+
+            Text(example)
+                .foregroundColor(.blue)
+                .font(.body)
+                .fixedSize(horizontal: false, vertical: true)
+        }
+    }
+}
+
+struct DataStructuresDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DataStructuresDetailView()
+    }
+}
+
---
+++ SwiftInstruct/Views/SwiftUIDetailView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct SwiftUIDetailView: View {
+    var modules: [ModuleData] = SwiftUIProgrammingData.modules
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    ForEach(modules, id: \.title) { module in
+                        categoryView(title: module.title, description: module.description, example: module.example)
+                    }
+                }
+                .padding()
+            }
+        }
+        .padding(.horizontal)
+    }
+
+    func categoryView(title: String, description: String, example: String) -> some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(title)
+                .font(.headline)
+
+            Text(description)
+                .foregroundColor(.secondary)
+                .fixedSize(horizontal: false, vertical: true)
+
+            Spacer()
+
+            Text(example)
+                .foregroundColor(.blue)
+                .font(.body)
+                .fixedSize(horizontal: false, vertical: true)
+        }
+    }
+}
+
+struct SwiftUIDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIDetailView()
+    }
+}
