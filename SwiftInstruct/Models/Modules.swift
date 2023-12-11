ad122529a8ab273d1e5a0bb3b90a69ec3844252b
Joshua Villareal <jvillareal@csu.fullerton.edu>
2023-12-11 01:17:49 -0800

    Added Shadan's Module.swift

--- SwiftInstruct/Models/Modules.swift
+++ SwiftInstruct/Models/Modules.swift
@@ -1,17 +1,24 @@
-import Foundation
+/* This will be the struct with all the module informations */
 
-struct ModuleData {
-    let title: String
-    let description: String
-    let example: String
-}
+import Foundation
+import SwiftUI
 
 struct Modules: Identifiable {
     let id: UUID
     var title: String
     var progress: Float
     var theme: Theme
-
+    
+    var progressCheck: String {
+        return progress == 1 ? "checkmark.circle.fill" :
+               progress > 0 ? "arrow.right.circle.fill" : "x.circle.fill"
+    }
+    
+    var progressTheme: Color {
+        return progress == 1 ? .green :
+               progress > 0 ? .orange : .red
+    }
+    
     init(id: UUID = UUID(), title: String, progress: Float, theme: Theme) {
         self.id = id
         self.title = title
@@ -20,12 +27,27 @@ struct Modules: Identifiable {
     }
 }
 
+struct ModuleData{
+    let title: String
+    let description: String
+    let example: String
+}
+
 extension Modules {
-    static let sampleModule: [Modules] = [
-        Modules(title: "Module 1: Swift Basics", progress: 1.00, theme: .charcoal),
-        Modules(title: "Module 2: Object-Oriented Programming", progress: 0.4572, theme: .charcoal),
-        Modules(title: "Module 3: Data Structures", progress: 0.00, theme: .charcoal),
-        Modules(title: "Module 4: Swift UI", progress: 0.00, theme: .charcoal)
+    static let sampleModule: [Modules] =
+    [
+        Modules(title: "Module 1: Swift Basics",
+                progress: 1.00,
+                theme: .charcoal),
+        Modules(title: "Module 2: Object-Oriented Programming",
+                progress: 0.4572,
+                theme: .charcoal),
+        Modules(title: "Module 3: Data Structures",
+                progress: 0.00,
+                theme: .charcoal),
+        Modules(title: "Module 4: Swift UI",
+                progress: 0.00,
+                theme: .charcoal)
     ]
 }
 
