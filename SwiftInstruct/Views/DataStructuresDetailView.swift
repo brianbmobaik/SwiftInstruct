603ec4c8859b8491918d105d2ab54b226cef768e
Joshua Villareal <112414450+Onceuuu@users.noreply.github.com>
2023-12-11 02:51:35 -0800

    Added background

--- SwiftInstruct/Views/DataStructuresDetailView.swift
+++ SwiftInstruct/Views/DataStructuresDetailView.swift
@@ -4,18 +4,21 @@ struct DataStructuresDetailView: View {
     var modules: [ModuleData] = DataStructuresData.modules
 
     var body: some View {
-        VStack(alignment: .leading) {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 20) {
-                    ForEach(modules, id: \.title) { module in
-                        categoryView(title: module.title, description: module.description, example: module.example)
+        ZStack{
+            BackgroundView()
+            VStack(alignment: .leading) {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        ForEach(modules, id: \.title) { module in
+                            categoryView(title: module.title, description: module.description, example: module.example)
+                        }
                     }
+                    .padding()
                 }
-                .padding()
             }
+            .padding(.horizontal)
         }
-        .padding(.horizontal)
-        // .navigationBarTitle("Data Structures")
+        
     }
 
     func categoryView(title: String, description: String, example: String) -> some View {
